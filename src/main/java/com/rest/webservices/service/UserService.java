8bee4f7307c6e87d7a0c7f2e4f41f1a9b9abdeda
nemgardose <gardosenemwel@gmail.com>
2020-09-16 01:12:50 +0800

    Made the Sign-up Web Service Endpoint Public

--- src/main/java/com/rest/webservices/service/UserService.java
+++ src/main/java/com/rest/webservices/service/UserService.java
@@ -1,8 +1,10 @@
 package com.rest.webservices.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.rest.webservices.shared.dto.UserDTO;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	UserDTO createUser(UserDTO user);
 
 }
