ed9a75002adc06c2e6447635859201cffeed5a08
nemgardose <gardosenemwel@gmail.com>
2020-09-20 00:35:12 +0800

    Customized Authentication Filter for Login

--- src/main/java/com/rest/webservices/security/WebSecurity.java
+++ src/main/java/com/rest/webservices/security/WebSecurity.java
@@ -26,14 +26,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
 		.permitAll()
 		.anyRequest()
-		.authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+		.authenticated().and().addFilter(getAuthenticationFilter());
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
-
-
-
+	
+	public AuthenticationFilter getAuthenticationFilter() throws Exception {
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
+	}
 }
